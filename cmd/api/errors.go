28a3f59e2838562bc43ec283ab062d7cb1abd706
Patrick Fanella <fanella.patrick@gmail.com>
2024-12-28 17:30:43 -0600

    feat: gitignore

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -10,3 +10,15 @@ func (app *application) internalServerError(w http.ResponseWriter, r *http.Reque
 	
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
 }
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bed request error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error() )
+	
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error() )
+	
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -10,16 +10,21 @@ import (
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return 
 	}
 
 	post := &store.Post{
@@ -55,7 +60,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
-			writeJSONError(w, http.StatusNotFound, err.Error())
+			app.notFoundResponse(w, r, err)
 		default:
 			app.internalServerError(w, r, err)
 		}
